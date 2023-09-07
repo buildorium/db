e6ac9c94bc072c30fb6045a7962efda6199af84b
rs-duongnt1 <87459579+rs-duongnt1@users.noreply.github.com>
2023-09-07 11:53:01 +0700

    Update mysql.go

--- mysql/mysql.go
+++ mysql/mysql.go
@@ -7,8 +7,8 @@ import (
 	"gorm.io/gorm"
 )
 
-func DB(dbName string) (*gorm.DB, error) {
-	dsn := fmt.Sprintf("root:root@tcp(mysql-%s:3306)/default?charset=utf8&parseTime=True&loc=Local", dbName)
+func DB(appName string, dbName string) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("root:root@tcp(mysql-%s-%s:3306)/default?charset=utf8&parseTime=True&loc=Local", appName, dbName)
 	db, err := gorm.Open(mysql.New(mysql.Config{
 		DSN: dsn,
 	}))
